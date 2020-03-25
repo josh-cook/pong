145aa02e9e515859c4a4d6e81205d170627f3f44
Josh Cook <cookiej91@gmail.com>
2020-03-25 13:00:55 +0000

    Add game initialiser canvas

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
-window.onload = function() {
+const canvas = document.getElementById("gameCanvas");
+const canvasContext = canvas.getContext("2d");
+
+function initialiseGame() {
   console.log("Hello world");
-};
+  console.log(canvas);
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+initialiseGame();
