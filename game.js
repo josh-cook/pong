e5a13cf0706d9c636049e7199eed225663b43114
Josh Cook <cookiej91@gmail.com>
2020-03-25 13:00:55 +0000

    Add flex to centre game

--- game.js
+++ game.js
@@ -2,10 +2,14 @@ const canvas = document.getElementById("gameCanvas");
 const canvasContext = canvas.getContext("2d");
 
 function initialiseGame() {
-  console.log("Hello world");
-  console.log(canvas);
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasContext.fillStyle = "white";
+  canvasContext.fillRect(200, 200, 50, 25);
+
+  canvasContext.fillStyle = "white";
+  canvasContext.fillRect(400, 300, 10, 30);
 }
 
 initialiseGame();
